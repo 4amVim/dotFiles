5230e43afdbea038d7d03796a1050d1d337e76ab
mathmetal <4429609+mathmetal@users.noreply.github.com>
2021-02-12 10:35:20 +0530

    dart lsp works

--- doom/config.el
+++ doom/config.el
@@ -69,3 +69,45 @@
 (add-hook 'after-change-major-mode-hook #'doom-modeline-conditional-buffer-encoding)
 
 (setq doom-themes-treemacs-theme "doom-colors")
+
+
+
+
+
+
+;; Install use-package
+(condition-case nil
+    (require 'use-package)
+  (file-error
+   (require 'package)
+   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+   (package-initialize)
+   (package-refresh-contents)
+   (package-install 'use-package)
+   (require 'use-package)))
+
+(use-package lsp-mode :ensure t)
+(use-package lsp-dart
+  :ensure t
+  :hook (dart-mode . lsp))
+
+;; Optional packages
+(use-package projectile :ensure t) ;; project management
+(use-package yasnippet
+  :ensure t
+  :config (yas-global-mode)) ;; snipets
+(use-package lsp-ui :ensure t) ;; UI for LSP
+(use-package company :ensure t) ;; Auto-complete
+
+;; Optional Flutter packages
+(use-package hover :ensure t) ;; run app from desktop without emulator
+
+;(require 'dart-mode)
+;(defun auto-reload ()
+;  (shell-command "kill -s SIGUSR1 $(cat /tmp/flutter.pid) && echo 'reload flutter...'"))
+;
+;(add-hook 'dart-mode-hook
+;          (lambda ()
+;            (add-hook 'after-save-hook 'auto-reload nil 'make-it-local)))
+
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
